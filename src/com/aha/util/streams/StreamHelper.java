6d6bad46d41478a3088d3b2c6fe9016096cae2a0
Armin Häberling <armin.aha@gmail.com>
2010-08-11 21:16:58 +0200

    filter utf8 BOM marker

--- src/com/aha/util/streams/StreamHelper.java
+++ src/com/aha/util/streams/StreamHelper.java
@@ -7,12 +7,21 @@ import java.io.InputStreamReader;
 
 public class StreamHelper {
 
+    private static final int BOM_MARKER = 65279;
+
     public static String readAll(InputStream in, String encoding) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in, encoding));
         StringBuilder result = new StringBuilder();
         String line = reader.readLine();
+        // filter unicode BOM markers
+        // see http://groups.google.com/group/comp.lang.java.programmer/browse_frm/thread/020adb9d6ad25412
+        int firstChar = (int)line.charAt(0);
+        if (firstChar == BOM_MARKER) {
+            line = line.substring(1);
+        }
         while (line != null) {
             result.append(line);
+            result.append("\n");
             line = reader.readLine();
         }
         return result.toString();
